64da12b92f1ad54020ca61ca7bcf1c35667a5ebc
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-11-30 17:36:59 +0100

    Added Form logic to generate DTO for search

--- flight.search.client/src/App.tsx
+++ flight.search.client/src/App.tsx
@@ -1,68 +1,65 @@
-import './App.css';
-import FlightListRound from './Compartments/FlightListRound';
-import FlightList from './Compartments/FlightList';
-import {useState} from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "./App.css";
+import FlightListRound from "./Compartments/FlightListRound";
+import FlightList from "./Compartments/FlightList";
+import React, { useState, useEffect } from "react";
+import { useForm, SubmitHandler,Controller } from "react-hook-form";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
+
+
+type SearchForm = {
+	DepartureDestination: string;
+	ArrivalDestination: string;
+	DepartureDate: Date;
+	RoundTrip: boolean;
+	RetrunDepartureDate: Date;
+};
 
 function App() {
-  const [SeatchForm, setSearchForm]= useState();
-  const [RoundTripState, setRooundTripState] = useState(true);
+	const [RoundTripState, setRooundTripState] = useState(true);
+  const [Shown, setShown] = useState(true);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<SearchForm>();
+
   
+	const onSubmit: SubmitHandler<SearchForm> = (data) => console.log(data);
+	
 
-const handleRoundTripState = () => {
-
-  setRooundTripState(!RoundTripState)
-}
-const handleFormSubmit = () => {
-
-}
-
-  return (
-
-    <>
-    <form onSubmit={handleFormSubmit}>
-    <label>
-        Departure     
-      <input type="text" name="destination" />
-      </label>
-      <label>
-        Destination     
-      <input type="text" name="arrival" />
-      </label>
-      <label>
-        Departure Date     
-      <input type="date" name="departureDate" />
-      </label>
-      <label>
-        Round trip     
-      <input type="checkbox" name="roundtrip" onChange= {handleRoundTripState } />
-      </label>
-      <label hidden={RoundTripState} >
-        Retrun date     
-      <input type="date" name="retrunDepartureDate" />
-      </label>
-      <input type="submit" />
-    </form>
-    <FlightList/>
-    <FlightListRound/>
-    </>
-       
-  );
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<label>
+					Departure:
+					<input type="text" defaultValue="" {...register("DepartureDestination", { required: true })} />
+				</label>
+				<label>
+					Destination:
+					<input type="text" defaultValue="" {...register("ArrivalDestination", { required: true })} />
+				</label>
+				<label>
+					Departure Date:
+					<input type="date" defaultValue="" {...register("DepartureDate", { required: true })}/>
+				</label>
+				<label>
+					Round trip
+          <input type="checkbox" defaultValue="" {...register("RoundTrip", { 
+            onChange: (e) => {setShown(!Shown)}, 
+              required: false })}/>
+				</label>        
+				<label  hidden={Shown}>
+					Return date:
+					<input type="date"  {...register("RetrunDepartureDate", { required: false })} />
+				</label>       
+        <input type="submit"/>
+			</form>
+			<FlightList />
+			<FlightListRound />
+		</>
+	);
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
